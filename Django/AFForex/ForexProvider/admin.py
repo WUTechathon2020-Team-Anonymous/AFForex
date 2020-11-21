b42d43e8a08b166e5471ab7dac7ce9a29480cf5b
Rishi Mundada <rishim1402@gmail.com>
2020-11-21 12:59:04 +0530

    Added databases for high and low of Buying Forex

--- Django/AFForex/ForexProvider/admin.py
+++ Django/AFForex/ForexProvider/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import ForexProvider
+from .models import ForexProvider,Buy_Cash_Low,Buy_Cash_High
 
 # Register your models here.
 admin.site.register(ForexProvider)
+admin.site.register(Buy_Cash_Low)
+admin.site.register(Buy_Cash_High)
